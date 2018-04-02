bbda8ac33430504b03bacaf6ad953c66e22cf48e
Daniel Tobon <daniel.tobon@nebulae.com.co>
2018-04-02 11:58:48 -0500

    - Se agregan logos de tpm - Se habilita logout

--- frontend/emi/src/app/main/toolbar/toolbar.component.ts
+++ frontend/emi/src/app/main/toolbar/toolbar.component.ts
@@ -6,98 +6,98 @@ import { KeycloakService } from 'keycloak-angular';
 import { KeycloakProfile } from 'keycloak-js';
 
 @Component({
-    selector: 'fuse-toolbar',
-    templateUrl: './toolbar.component.html',
-    styleUrls: ['./toolbar.component.scss']
+  selector: 'fuse-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.scss']
 })
-
 export class FuseToolbarComponent {
-    userDetails: KeycloakProfile;
-    userStatusOptions: any[];
-    languages: any;
-    selectedLanguage: any;
-    showLoadingBar: boolean;
-    horizontalNav: boolean;
+  userDetails: KeycloakProfile;
+  userStatusOptions: any[];
+  languages: any;
+  selectedLanguage: any;
+  showLoadingBar: boolean;
+  horizontalNav: boolean;
 
-    constructor(
-        private router: Router,
-        private fuseConfig: FuseConfigService,
-        private translate: TranslateService,
-        private keycloakService: KeycloakService
-    ) {
-        this.userStatusOptions = [
-            {
-                'title': 'Online',
-                'icon': 'icon-checkbox-marked-circle',
-                'color': '#4CAF50'
-            },
-            {
-                'title': 'Away',
-                'icon': 'icon-clock',
-                'color': '#FFC107'
-            },
-            {
-                'title': 'Do not Disturb',
-                'icon': 'icon-minus-circle',
-                'color': '#F44336'
-            },
-            {
-                'title': 'Invisible',
-                'icon': 'icon-checkbox-blank-circle-outline',
-                'color': '#BDBDBD'
-            },
-            {
-                'title': 'Offline',
-                'icon': 'icon-checkbox-blank-circle-outline',
-                'color': '#616161'
-            }
-        ];
+  constructor(
+    private router: Router,
+    private fuseConfig: FuseConfigService,
+    private translate: TranslateService,
+    private keycloakService: KeycloakService
+  ) {
+    this.userStatusOptions = [
+      {
+        title: 'Online',
+        icon: 'icon-checkbox-marked-circle',
+        color: '#4CAF50'
+      },
+      {
+        title: 'Away',
+        icon: 'icon-clock',
+        color: '#FFC107'
+      },
+      {
+        title: 'Do not Disturb',
+        icon: 'icon-minus-circle',
+        color: '#F44336'
+      },
+      {
+        title: 'Invisible',
+        icon: 'icon-checkbox-blank-circle-outline',
+        color: '#BDBDBD'
+      },
+      {
+        title: 'Offline',
+        icon: 'icon-checkbox-blank-circle-outline',
+        color: '#616161'
+      }
+    ];
 
-        this.languages = [
-            {
-                'id': 'en',
-                'title': 'English',
-                'flag': 'us'
-            },
-            {
-                'id': 'es',
-                'title': 'Español',
-                'flag': 'es'
-            }
-        ];
+    this.languages = [
+      {
+        id: 'en',
+        title: 'English',
+        flag: 'us'
+      },
+      {
+        id: 'es',
+        title: 'Español',
+        flag: 'es'
+      }
+    ];
 
-        this.selectedLanguage = this.languages[0];
+    this.selectedLanguage = this.languages[0];
 
-        router.events.subscribe(
-            (event) => {
-                if (event instanceof NavigationStart) {
-                    this.showLoadingBar = true;
-                }
-                if (event instanceof NavigationEnd) {
-                    this.showLoadingBar = false;
-                }
-            });
+    router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.showLoadingBar = true;
+      }
+      if (event instanceof NavigationEnd) {
+        this.showLoadingBar = false;
+      }
+    });
 
-        this.fuseConfig.onSettingsChanged.subscribe((settings) => {
-            this.horizontalNav = settings.layout.navigation === 'top';
-        });
+    this.fuseConfig.onSettingsChanged.subscribe(settings => {
+      this.horizontalNav = settings.layout.navigation === 'top';
+    });
+  }
 
-    }
+  async ngOnInit() {
+    this.userDetails = await this.keycloakService.loadUserProfile();
+  }
 
-    async ngOnInit() {
-        this.userDetails = await this.keycloakService.loadUserProfile();
-    }
+  logout() {
+    this.keycloakService.logout();
+  }
+  search(value) {
+    // Do your search here...
+    console.log(value);
+  }
 
-    search(value) {
-        // Do your search here...
-        console.log(value);
-    }
+  setLanguage(lang) {
+    // Set the selected language for toolbar
+    this.selectedLanguage = lang;
 
-    setLanguage(lang) {
-        // Set the selected language for toolbar
-        this.selectedLanguage = lang;
-
-        // Use the selected language for translations
-        this.translate.use(lang.id);
-    }
+    // Use the selected language for translations
+    this.translate.use(lang.id);
+  }
 }

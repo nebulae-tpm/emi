1cf9f8df3839d1405ab94086feb7a1a39683b331
Daniel Tobon <daniel.tobon@nebulae.com.co>
2018-05-28 15:47:48 -0500

    Se agrega internacionalizacion de paginador de tabla

--- frontend/emi/src/app/main/i18n/en.ts
+++ frontend/emi/src/app/main/i18n/en.ts
@@ -1,13 +1,17 @@
 export const locale = {
   lang: 'en',
   data: {
-      'TOOLBAR': {
-        'MY_PROFILE': 'My Profile',
-        'SEARCH': 'Search',
-        'LOGOUT': 'Logout',
-        'SHORTCUT_TOOLTIP': 'Click to add/remove shortcut',
-        'SHORTCUT_SEARCH': 'Search for an app or a page',
-        'SHORTCUT_EMPTY': 'No shortcuts yet!',
-      }
+    TOOLBAR: {
+      MY_PROFILE: 'My Profile',
+      SEARCH: 'Search',
+      LOGOUT: 'Logout',
+      SHORTCUT_TOOLTIP: 'Click to add/remove shortcut',
+      SHORTCUT_SEARCH: 'Search for an app or a page',
+      SHORTCUT_EMPTY: 'No shortcuts yet!'
+    },
+    ITEMS_PER_PAGE: 'Item per page',
+    NEXT_PAGE: 'Next page',
+    PREVIOUS_PAGE: 'Previous page',
+    OF_LABEL: 'of'
   }
 };

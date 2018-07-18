15f93a8b88ef4e55be2f8b7cd2a0de0be7d4ce8e
Daniel Tobon <daniel.tobon@nebulae.com.co>
2018-07-18 09:05:49 -0500

    Se crea compatibilidad con angular 6 y se implementa PWA

--- frontend/emi/src/environments/environment.prod.ts
+++ frontend/emi/src/environments/environment.prod.ts
@@ -1,22 +1,22 @@
 export const environment = {
-    "production": true,
-    "hmr": false,
-    "keycloak": {
-        "url": "https://iot.nebulae.com.co/auth",
-        "realm": "TPM",
-        "clientId": "EMI",
-        "onLoad": "login-required",
-        "checkLoginIframe": false,
-        "bearerExcludedUrls": [
-            "/assets"
+    'production': true,
+    'hmr': false,
+    'keycloak': {
+        'url': 'https://iot.nebulae.com.co/auth',
+        'realm': 'TPM',
+        'clientId': 'EMI',
+        'onLoad': 'login-required',
+        'checkLoginIframe': false,
+        'bearerExcludedUrls': [
+            '/assets'
         ]
     },
-    "api": {
-        "gateway": {
-            "graphql": {
-                "httpEndPoint": "https://iot.nebulae.com.co/api/gateway/graphql/http",
-                "wsEndPoint": "wss://iot.nebulae.com.co/api/gateway/graphql/ws",
-                "graphiqlEndPoint": "https://iot.nebulae.com.co/api/gateway/graphiql"
+    'api': {
+        'gateway': {
+            'graphql': {
+                'httpEndPoint': 'https://iot.nebulae.com.co/api/gateway/graphql/http',
+                'wsEndPoint': 'wss://iot.nebulae.com.co/api/gateway/graphql/ws',
+                'graphiqlEndPoint': 'https://iot.nebulae.com.co/api/gateway/graphiql'
             }
         }
     }
